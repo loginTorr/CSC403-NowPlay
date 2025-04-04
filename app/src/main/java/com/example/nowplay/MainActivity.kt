18f2366304f5375196ec1b332b9ad832109126b9
Tommy Wallace <tommy3215@live.com>
2025-04-04 01:18:48 -0500

    Added Navigation Bar. MainActivity.kt has 4 errors but it runs just fine. Also commented out the required .json file from the .gitignore.

--- app/src/main/java/com/example/nowplay/MainActivity.kt
+++ app/src/main/java/com/example/nowplay/MainActivity.kt
@@ -1,47 +1,157 @@
 package com.example.nowplay
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.nowplay.ui.theme.NowPlayTheme
 
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null,
+    val showLabel: Boolean
+)
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NowPlayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false,
+                        showLabel = true,
+                    ),
+                    BottomNavigationItem(
+                        title = "Friends",
+                        selectedIcon = Icons.Filled.Favorite,
+                        unselectedIcon = Icons.Outlined.FavoriteBorder,
+                        hasNews = false,
+                        showLabel = true,
+                    ),
+                    BottomNavigationItem(
+                        title = "Post",
+                        selectedIcon = Icons.Filled.AddCircle,
+                        unselectedIcon = Icons.Outlined.AddCircle,
+                        hasNews = false,
+                        showLabel = false,
+                    ),
+                    BottomNavigationItem(
+                        title = "Chat",
+                        selectedIcon = Icons.Filled.Email,
+                        unselectedIcon = Icons.Outlined.Email,
+                        hasNews = false,
+                        badgeCount = null,
+                        showLabel = true,
+                    ),
+                    BottomNavigationItem(
+                        title = "Profile",
+                        selectedIcon = Icons.Filled.AccountCircle,
+                        unselectedIcon = Icons.Outlined.AccountCircle,
+                        hasNews = false,
+                        showLabel = true,
+                    ),
+                )
+                    var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Scaffold(
+                        containerColor = Color(26,27,28),
+                        bottomBar = {
+                            NavigationBar(
+                                contentColor = Color.White,
+                                containerColor = Color.DarkGray,
+                            ) {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            /* TO DO (Tommy): Navigation Controller still needs
+                                             to be created in order to create the actual individual
+                                             tab pages */
+                                            //navController.navigate(item.title)
+                                        },
+                                        label = {
+                                            if (item.showLabel) {
+                                                Text(
+                                                    text = item.title,
+                                                    color = Color.White
+                                                )
+                                            }
+                                        },
+                                        icon = {
+                                            BadgedBox(
+                                                badge = {
+                                                    if (item.badgeCount != null) {
+                                                        Badge {
+                                                            Text(text = item.badgeCount.toString())
+                                                        }
+                                                    } else if (item.hasNews) {
+                                                        Badge()
+                                                    }
+                                                }
+                                            ) {
+                                                Icon(
+                                                    imageVector = if (index == selectedItemIndex) {
+                                                        item.selectedIcon
+
+                                                    } else item.unselectedIcon,
+                                                    contentDescription = item.title,
+                                                    tint = if (index == selectedItemIndex) {
+                                                        Color.DarkGray
+                                                    } else Color.White
+                                                )
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    ){
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NowPlayTheme {
-        Greeting("Android")
     }
 }
